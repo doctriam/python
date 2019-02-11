30af8e3e17504b3d199804c4a8f41eda413b614d
doctriam <hayniekw@gmail.com>
2019-02-11 00:00:25 -0500

    I's hit the bed

--- tutorials/helloworld.py
+++ tutorials/helloworld.py
@@ -11,53 +11,79 @@ EDITOR:  VIM 8.0.1453 """
 
 import sys
 
-def endproc():
-    """Function for prompting user input and deleting prompt after Enter is pressed"""
+CURSOR_UP='\x1b[1A'
+ERASE_LINE='\x1b[2K'
+
+def main_menu():
+    print("1. Hello world")
+    print("2. Print variable")
+    print("3. Multiple Assignments")
+    print("4. Lists")
+    print("5. Modulus")
+    print("6. Exponents")
+    print("0. Exit")
+    menuOption=int(input("Choose a number from the menu above:"))
+    options ={1: hello_world,
+              2: print_variable,
+              3: multiple_assignments,
+              4: list_append,
+              5: modulus_function,
+              6: exponent_function
+             }
+    for n in range(0,len(options)+2):
+        sys.stdout.write(CURSOR_UP)
+        sys.stdout.write(ERASE_LINE)
     print()
-    input("Press Enter to continue")
-    cursor_up = '\x1b[1A'
-    erase_line = '\x1b[2K'
-    sys.stdout.write(cursor_up)
-    sys.stdout.write(erase_line)
+    if menuOption in range(1,len(options)+1):
+        options[menuOption]()
+    else:
+        sys.exit()
+    print(),print()
+    main_menu()
 
 # Hello World
-print("Hello, World!")
-endproc()
+def hello_world():
+    print("Prints 'Hello, World!")
+    print("Hello, World!")
 
 # Print variable
-X = 1
-if X == 1:
-    print("X is equal to %s" % X)
-endproc()
+def print_variable():
+    print("Prints a string with an inserted variable")
+    X = 1
+    if X == 1:
+        print("X is equal to %s" % X)
 
 # Multiple assignments
-A, B = 3, 4
-print("Multiple variables assigned on one line")
-print(A, B)
-endproc()
+def multiple_assignments():
+    A, B = 3, 4
+    print("Multiple variables assigned on one line")
+    print(A, B)
 
 # Lists
-print("List of numbers 1,2,3 using individual prints")
-MYLIST = []
-MYLIST.append(1)
-MYLIST.append(2)
-MYLIST.append(3)
-print(MYLIST[0])
-print(MYLIST[1])
-print(MYLIST[2])
-print("List of numbers 1,2,3 using a for statement")
-for x in MYLIST:
-    print(x)
-endproc()
+def list_append():
+    print("List of numbers 1,2,3 using individual prints")
+    MYLIST = []
+    MYLIST.append(1)
+    MYLIST.append(2)
+    MYLIST.append(3)
+    print(MYLIST[0])
+    print(MYLIST[1])
+    print(MYLIST[2])
+    print("List of numbers 1,2,3 using a for statement")
+    for x in MYLIST:
+        print(x)
 
 # MODULUS
-print("Remainder of 11/3")
-MODULUS = 11 % 3
-print(MODULUS)
-endproc()
+def modulus_function():
+    print("Perform a modulus function")
+    print("Remainder of 11/3")
+    MODULUS = 11 % 3
+    print(MODULUS)
 
 # Exponents
-print("Exponents")
-CUBED = 4 ** 3
-print("4^3 = %s" % CUBED)
-endproc()
+def exponent_function():
+    print("Exponents")
+    CUBED = 4 ** 3
+    print("4^3 = %s" % CUBED)
+
+main_menu()
